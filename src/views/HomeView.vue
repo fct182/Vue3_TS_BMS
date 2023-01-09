140a8d991f3df4d3233a0f09cbb3dc55c6930e21
fct <CHANGTAO.FU@HAND-CHINA.COM>
2023-01-09 10:07:27 +0800

    feat(package包): 增加commitizen工具，统一代码提交风格

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,11 +11,6 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
-
-
-
-
-
   components: {
     HelloWorld
   }
